184942d16ab045e79aff24fa030fbffe1a2dbfe0
Javier Pereyra <javier.pereyra@gmail.com>
2020-10-04 19:20:58 -0300

    ventanas y content pane

--- src/presentacion/vista/VentanaPrincipal.java
+++ src/presentacion/vista/VentanaPrincipal.java
@@ -2,7 +2,12 @@ package presentacion.vista;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Locale;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -18,33 +23,76 @@ public class VentanaPrincipal extends JFrame {
 	 * Create the frame.
 	 */
 	public VentanaPrincipal() {
+		
+		setTitle("Ingreso Personas");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		
+		// JMenuBar
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
+		// Jmenu
 		JMenu mnPersonas = new JMenu("Personas");
+		mnPersonas.setFont(new Font("Segoe UI", Font.PLAIN, 18));
 		menuBar.add(mnPersonas);
-		
+
 		JMenuItem mntmAgregarPersona = new JMenuItem("Agregar");
 		mnPersonas.add(mntmAgregarPersona);
 		
+		mntmAgregarPersona.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
+				contentPane.removeAll();
+				//Abre el formulario Panel Ingreso Persona dentro de la ventana principal
+				PanelAgregarPersona panel = new PanelAgregarPersona();
+				System.out.println("nuevo objeto panel agregar");
+				contentPane.add(panel);
+				contentPane.repaint();
+				contentPane.revalidate();
+			}
+			
+		});
+		
+
 		JMenuItem mntmEliminarPersona = new JMenuItem("Eliminar");
 		mnPersonas.add(mntmEliminarPersona);
+		mntmEliminarPersona.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			
+			contentPane.removeAll();
+			//Abre el formulario Panel Eliminar Persona dentro de la ventana principal
+			PanelEliminarPersona panel = new PanelEliminarPersona();
+			System.out.println("nuevo objeto panel eliminar");
+			contentPane.add(panel);
+			contentPane.repaint();
+			contentPane.revalidate();
+		}
 		
+	});
+		
+
 		JMenuItem mntmModificarPersona = new JMenuItem("Modificar");
 		mnPersonas.add(mntmModificarPersona);
-		
+
 		JMenuItem mntmListarPersonas = new JMenuItem("Listar");
 		mnPersonas.add(mntmListarPersonas);
+
+//		contentPane = new JPanel();
+//		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+//		setContentPane(contentPane);
+//		contentPane.setLayout(null);
+//
+//		JPanel panel = new JPanel();
+//		panel.setBounds(424, 230, -411, -219);
+//		contentPane.add(panel);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(424, 230, -411, -219);
-		contentPane.add(panel);
+		
 	}
+
 }
